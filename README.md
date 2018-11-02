066996ffba8f2f88e12f500bf462e655a0768027
Sheila Stephen <sheila.je@gmail.com>
2018-11-02 15:05:47 -0700

    make teh inputted value get displayed on the screen as a bill

--- README.md
+++ README.md
@@ -22,7 +22,7 @@ _No Known Bugs_
 
 ## Specs
 
-* _[] Create a constructor - pizza , with properties size and toppings._
+* _[X] Create a constructor - pizza , with properties size and toppings._
 * _[] Create a prototype calculateCost based on the size and the number of toppings selected by the User. Find the length of the Toppings array and get the size of the pizza.if number of toppings is 2 ,its price is $5. If pizza chosen by the user is size s, then its price is $4_
     * _Example Input :_
                   * _Number of toppings = 2_
--- js/scripts.js
+++ js/scripts.js
@@ -7,7 +7,7 @@ Pizza.prototype.addToppings = function(topping){
   this.toppings.push(topping);
 }
 
-Pizza.prototype.toppingsBasedAmount = function(){
+Pizza.prototype.toppingsBasedAmount = function(noOfToppings){
   if (noOfToppings === 4){
     return 7;
   } else if (noOfToppings === 3){
@@ -16,35 +16,34 @@ Pizza.prototype.toppingsBasedAmount = function(){
     return 5;
   } else if (noOfToppings === 1){
     return 4;
+    alert("4")
   }else {
     return 0;
     alert ("Please select the no. of toppings");
   }
 }
 
-Pizza.prototype.sizeBasedAmount = function(){
-  if (size == "s"){
+Pizza.prototype.sizeBasedAmount = function(size){
+  if (size == "Small"){
     return 4;
-  } else if(size == "M"){
+  } else if(size == "Medium"){
     return 5;
-  } else if(size == "L"){
+  } else if(size == "Large"){
     return 6;
-  }else if(size == "XL"){
-    return 7;
   } else {
     return 0;
     alert ("Please select a size that you would prefer");
   }
 }
 
-Pizza.prototype.calculateCost = function(tBasedAmt, size){
+Pizza.prototype.calculateCost = function(tBasedAmt, sizeBasedAmt){
   var totalCost = this.noOfToppings + this.size;
+  $("#displayTotalCost").text(totalCost);
 }
 
 $(document).ready(function(){
   $("#formid").submit(function(event){
     event.preventDefault();
-    var pizza = new Pizza();
     var inputtedName = $("#name").val();
     var inputtedNoToppings = parseInt($("#noToppings").val());
     $("input:checkbox[name=toppingCheck]:checked").each(function(){
@@ -54,6 +53,11 @@ $(document).ready(function(){
    var inputtedSize = $("input:radio[name=sizePizza]").val();
    $("#displayName").text(inputtedName);
    $("#displaySize").append(inputtedSize);
+
+   var pizza = new Pizza();
+   var toppingBasedAmt = pizza.toppingsBasedAmount(inputtedNoToppings);
+   var sizeBasedAmt = pizza.sizeBasedAmount(inputtedSize);
+   pizza.calculateCost(toppingBasedAmt, sizeBasedAmt);
   });
 
 });
