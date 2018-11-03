217d1b39b8e523f526466d958f0caf87584f5a65
Sheila Stephen <sheila.je@gmail.com>
2018-11-02 17:13:55 -0700

    get the final output

--- js/scripts.js
+++ js/scripts.js
@@ -3,67 +3,47 @@ function Pizza(toppings, size){
   this.size = size
 }
 
-// Pizza.prototype.addToppings = function(topping){
-//   this.toppings.push(topping);
-// }
-
 Pizza.prototype.toppingsBasedAmount = function(toppings){
-  for (var i = 1; i <= toppings.length; i++){
-    if(toppings[i] != ""){
-      var toppingPrice = 0;
-      toppingPrice = i * 3;
+    if(toppings.length != 0){
+      var toppingPrice = toppings.length *3;
+      return toppingPrice;
+    }else {
+      alert("please choose your Toppings !!")
     }
-    return toppingPrice;
   }
-  // if (noOfToppings === 4){
-  //   return 7;
-  // } else if (noOfToppings === 3){
-  //   return 6;
-  // } else if (noOfToppings === 2){
-  //   return 5;
-  // } else if (noOfToppings === 1){
-  //   return 4;
-  //   alert("4")
-  // }else {
-  //   return 0;
-  //   alert ("Please select the no. of toppings");
-  // }
-}
 
 Pizza.prototype.sizeBasedAmount = function(size){
-  if (size == "Small"){
+  if (size === "Small"){
     return 4;
-  } else if(size == "Medium"){
+  } else if(size === "Medium"){
     return 5;
-  } else if(size == "Large"){
+  } else if(size === "Large"){
     return 6;
   } else {
-    return 0;
     alert ("Please select a size that you would prefer");
   }
 }
 
 Pizza.prototype.calculateCost = function(tBasedAmt, sizeBasedAmt){
-  var totalCost = this.tBasedAmt + parseInt(this.sizeBasedAmt);
+  var totalCost =tBasedAmt +sizeBasedAmt;
   $("#displayTotalCost").text(totalCost);
 }
-
 
 var pizza;
 $(document).ready(function(){
   $("#formid").submit(function(event){
     event.preventDefault();
+    $("#show-Bill").show();
     var inputtedName = $("#name").val();
     var inputtedNoToppings = parseInt($("#noToppings").val());
     var toppingArray = [];
     $("input:checkbox[name=toppingCheck]:checked").each(function(){
      var inputtedToppings = $(this).val();
      toppingArray.push(inputtedToppings);
-     //alert(pizza.toppings);
      $("#displayToppings").append(inputtedToppings + ",");
    });
 
-   var inputtedSize =$("input:radio[name=sizePizza]").val();
+   var inputtedSize =$("input:radio[name=sizePizza]:checked").val();
    $("#displayName").text(inputtedName);
    $("#displaySize").append(inputtedSize);
 
@@ -75,5 +55,4 @@ $(document).ready(function(){
    console.log(sizeBasedAmt);
    pizza.calculateCost(toppingBasedAmt, sizeBasedAmt);
   });
-
 });
